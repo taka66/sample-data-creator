cc9160baae79fd8084c1f4dc71cfafbc20b042e2
taka66 <taka66.iphone@gmail.com>
2016-01-13 23:51:20 +0900

    Add JavaDoc and rest docs dependency.

--- src/main/java/com/takahirofujii/condominium/CondominiumDataCreator.java
+++ src/main/java/com/takahirofujii/condominium/CondominiumDataCreator.java
@@ -9,35 +9,46 @@ import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
+
 @Service
 public class CondominiumDataCreator {
 
-	private static final List<String> PREFECTURE = new ArrayList<>(Arrays.asList("Tokyo","Kyoto","Osaka","Hokkaido"));
-	private static final List<String> NAME_SUFFIX = new ArrayList<>(Arrays.asList("Hills","House","Mansion"));	
+	private static final List<String> PREFECTURE = new ArrayList<>(
+			Arrays.asList("Tokyo", "Kyoto", "Osaka", "Hokkaido"));
+	private static final List<String> NAME_SUFFIX = new ArrayList<>(Arrays.asList("Hills", "House", "Mansion"));
 
 	/**
 	 * Create condominium master data.
+	 * 
 	 * @param numberOfData
 	 * @return List<Condominium>
 	 */
-	List<Condominium> createData(int numberOfData){
+	List<Condominium> createData(int numberOfData) {
 		List<Condominium> data = new ArrayList<Condominium>();
-		IntStream.range(1,numberOfData + 1)
-		.forEach(i -> data.add(new Condominium(i,RandomStringUtils.randomAlphabetic(5).concat(NAME_SUFFIX.get((int)(Math.random()*NAME_SUFFIX.size())))
-				,PREFECTURE.get((int)(Math.random()*PREFECTURE.size())))));
+		IntStream.range(1, numberOfData + 1)
+				.forEach(i -> data.add(new Condominium(i,
+						RandomStringUtils.randomAlphabetic(5)
+								.concat(NAME_SUFFIX.get((int) (Math.random() * NAME_SUFFIX.size()))),
+						PREFECTURE.get((int) (Math.random() * PREFECTURE.size())))));
 		return data;
 	}
-	
-	List<CondominiumSales> createSalesData(int numberOfData){
+
+	/**
+	 * Create comdominium sales data.
+	 * 
+	 * @param numberOfData
+	 * @return
+	 */
+	List<CondominiumSales> createSalesData(int numberOfData) {
 		List<CondominiumSales> data = new ArrayList<CondominiumSales>();
-		IntStream.range(1,numberOfData + 1).forEach(i -> data.addAll(createOneYearData(i)));
+		IntStream.range(1, numberOfData + 1).forEach(i -> data.addAll(createOneYearData(i)));
 		return data;
 	}
-	
-	private List<CondominiumSales> createOneYearData(long id){
+
+	private List<CondominiumSales> createOneYearData(long id) {
 		List<CondominiumSales> data = new ArrayList<CondominiumSales>();
-		IntStream.range(0, 12)
-				 .forEach(i -> data.add(new CondominiumSales(id,YearMonth.now().plusMonths(i),BigDecimal.valueOf((int)(Math.random()*1000000)))));
+		IntStream.range(0, 12).forEach(i -> data.add(new CondominiumSales(id, YearMonth.now().plusMonths(i),
+				BigDecimal.valueOf((int) (Math.random() * 1000000)))));
 		return data;
 	}
 }
